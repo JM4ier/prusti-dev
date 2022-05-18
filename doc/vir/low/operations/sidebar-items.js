dcd66e98826adc2257a0ebc81f640bac7e9b9601
JM4ier <JM4ier@users.noreply.github.com>
2022-05-18 11:02:09 +0000

    deploy: 56fe103fa3e6fa1b5949025ccc294b430aebaa7a

--- doc/vir/low/operations/sidebar-items.js
+++ doc/vir/low/operations/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["constants",""],["expression",""],["helpers",""],["method",""],["statements",""],["ty",""]],"trait":[["ToLow",""],["ToLowLowerer",""]]});
+initSidebarItems({"mod":[["constants",""],["expression",""],["graphviz",""],["helpers",""],["method",""],["position",""],["special_variables",""],["statements",""],["successor",""],["ty",""]]});
