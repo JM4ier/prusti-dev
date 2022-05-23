4abebe448d0110b22ccd87b4baf0529e4d893838
Jonas Maier <jonas.maier@inf.ethz.ch>
2022-05-23 15:50:57 +0200

    more tests

--- prusti-tests/tests/verify_overflow/fail/prusti_assert.rs
+++ prusti-tests/tests/verify_overflow/fail/prusti_assert.rs
@@ -13,17 +13,36 @@ fn failing_assert() {
     prusti_assert!(false); //~ ERROR
 }
 
-fn quantifiers() {
-    // FIXME: quantifier lowering isn't implemented with -Punsafe_core_proof
-    prusti_assert!(forall(|x: u32| x == x));
+fn assign() {
+    let mut i = 10;
+    prusti_assert!(i == 10);
+    i = i * 10;
+    prusti_assert!(i == 100);
+    i -= 100;
+    prusti_assert!(i == 2); // ~ERROR
 }
 
-fn loop_shouldnt_crash() {
-    // FIXME: loop actually crashes
-    loop {
-        prusti_assert!(true);
-        body_invariant!(true);
+fn implication(arg: i32) {
+    let x;
+    if arg == 0 {
+        x = 1;
+    } else {
+        x = -1;
     }
+    prusti_assert!((arg == 0 ==> x > 0) && (arg != 0 ==> x < 0));
 }
+
+//fn quantifiers() {
+//    // FIXME: quantifier lowering isn't implemented with -Punsafe_core_proof
+//    prusti_assert!(forall(|x: u32| x == x));
+//}
+
+//fn loop_shouldnt_crash() {
+//    // FIXME: loop actually crashes
+//    loop {
+//        prusti_assert!(true);
+//        body_invariant!(true);
+//    }
+//}
 
 fn main() {}
